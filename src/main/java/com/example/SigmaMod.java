d009870b36230b842886f9736ba36c5d3b8e7bc0
LYZX-EN <LYZX.130@proton.me>
2025-12-12 16:54:05 +0100

    Initial commit

---
+++ src/main/java/com/example/SigmaMod.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.world.GameMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SigmaMod implements ModInitializer {
+    public static final String MOD_ID = "sigmamod";
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register((livingEntity, damageSource, v) -> {
+            if (damageSource.isOf(DamageTypes.FALL) && livingEntity.isPlayer()) {
+                livingEntity.kill(null);
+            }
+            return true;
+        });
+
+        ServerPlayerEvents.AFTER_RESPAWN.register((original, newPlayer, b) -> {
+            newPlayer.changeGameMode(GameMode.SPECTATOR);
+        });
+    }
+}
